289b875dd7e8bbfa795d3ff7e6abd0419f17818e
doulgs <douglasdsdomiciano@hotmail.com>
2024-02-18 00:57:56 -0300

    :sparkles: feat: Página de Cadastro

---
+++ src/app/(tabs)/register.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function Register() {
+  return (
+    <View className="flex-1 bg-gray-50 items-center justify-center">
+      <Text className="text-black font-body">register</Text>
+    </View>
+  );
+}
