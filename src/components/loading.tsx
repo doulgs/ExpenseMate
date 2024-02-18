94cc231c6aa09ab0d0ff464922cfd20aaa81e2da
doulgs <douglasdsdomiciano@hotmail.com>
2024-02-18 00:59:32 -0300

    :sparkles: feat: Criação do componente Loading

---
+++ src/components/loading.tsx
@@ -0,0 +1,7 @@
+import { ActivityIndicator } from "react-native";
+
+export default function Loading() {
+  return (
+    <ActivityIndicator className="flex-auto bg-gray-900 items-center justify-center" />
+  );
+}
