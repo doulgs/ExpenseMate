72c93fe448fad504c8a2666c31ecd71cc38670ee
doulgs <douglasdsdomiciano@hotmail.com>
2024-02-18 00:57:41 -0300

    :sparkles: feat: Página de Extratos

---
+++ src/app/(tabs)/extract.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function Extract() {
+  return (
+    <View className="flex-1 bg-gray-50 items-center justify-center">
+      <Text className="text-black font-body">Extract</Text>
+    </View>
+  );
+}
